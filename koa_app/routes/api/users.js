4988d41eaf1ca177e27aff4a5f6a8a1cd2513f15
tenglai <13049322087@163.com>
2019-07-21 22:35:53 +0800

    add gravatar

--- koa_app/routes/api/users.js
+++ koa_app/routes/api/users.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router');
 const router = new Router();
 const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
 
 // 引入User
 const User = require('../../models/User');
@@ -30,10 +31,11 @@ router.post('/register', async ctx => {
     ctx.status = 500;
     ctx.body = { email: '邮箱已被占用 ' };
   } else {
-    // 没查到
+    const avatar = gravatar.url(ctx.request.body.email, { s: '200', r: 'pg', d: 'mm' });
     const newUser = new User({
       name: ctx.request.body.name,
       email: ctx.request.body.email,
+      avatar,
       password: ctx.request.body.password
     });
 
